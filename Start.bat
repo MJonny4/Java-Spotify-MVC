a63c3a23a18f4a6405fe442370c548ac5d6d94dc
Ion <Ion2000m@gmail.com>
2023-09-11 12:23:58 +0200

    Script Update From .bat to .ps1

--- Start.bat
+++
@@ -1,2 +0,0 @@
-cd "C:\Users\isard\Desktop\P8\P1_Vista\dist"
-java -jar P1_Vista.jar lib/*.jar
---
+++ Start.ps1
@@ -0,0 +1,4 @@
+$PATH = "C:\Users\isard\Desktop\P8\P1_Vista\dist"
+
+cd $PATH
+java -jar P1_Vista.jar lib/*.jar
